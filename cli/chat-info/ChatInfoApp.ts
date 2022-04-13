033262f3ceeeee0135ccca95743fc453ac52127f
BenderskiyMA <olmer@remlo.ru>
2022-04-14 01:26:00 +0700

    All fixed. All works.

--- cli/chat-info/ChatInfoApp.ts
+++ cli/chat-info/ChatInfoApp.ts
@@ -16,7 +16,12 @@ class InfoCommand implements ISlashCommand {
     public command = 'info';
     public i18nDescription = 'Команда выводит имя создателя, владельца и модераторов канала.';
     public providesPreview = true;
-    public i18nParamsExample = '';
+    public i18nParamsExample = 'Параметры не требуются';
+    public i18nChannelWord = 'Канал';
+    public i18nWasCreated = 'был создан';
+    public i18nModerators = 'Модераторы канала';
+    public i18nOwners = 'Владельцы канала';
+    public i18nWorkingOnlyInPrivate = 'команда работает только в приватных каналах';
 
     private readonly accessors: IAppAccessors;
     private readonly applogger: ILogger;
@@ -39,21 +44,22 @@ class InfoCommand implements ISlashCommand {
         // const roomRead: IRoomRead = this.accessors.reader.getRoomReader();
         const token: string = await this.accessors.reader.getEnvironmentReader().getSettings().getValueById('id_info_setting_token');
         const userid: string = await this.accessors.reader.getEnvironmentReader().getSettings().getValueById('id_info_setting_uid');
-        const moderators: Array<string> = await this.getuserlist(room.id, 'moderator', http, token, userid);
-        const owners: Array<string> = await this.getuserlist(room.id, 'owner', http, token, userid);
+        const hosturl: string = await this.accessors.reader.getEnvironmentReader().getSettings().getValueById('id_info_setting_host_url');
+        const moderators: Array<string> = await this.getuserlist(room.id, 'moderator', http, token, userid, hosturl);
+        const owners: Array<string> = await this.getuserlist(room.id, 'owner', http, token, userid, hosturl);
         let msgText: string;
         if ((room.type === 'p') && (room.displayName) && (room.creator)) {
-            msgText = 'Канал *"' + room.displayName + '"* был создан @' + room.creator?.username + '\n';
-            msgText += '*Модераторы канала:*\n' ;
+            msgText = this.i18nChannelWord + ' *"' + room.displayName + '"* ' + this.i18nWasCreated + ' @' + room.creator?.username + '\n';
+            msgText += '*' + this.i18nModerators + ':*\n';
             for (const moder of moderators) {
                 msgText += '@' + moder + '\n';
             }
-            msgText += '*Владельцы канала:*\n';
+            msgText += '*' + this.i18nOwners + ':*\n';
             for (const owner of owners) {
                 msgText += '@' + owner + '\n';
             }
         } else {
-            msgText = '/info команда работает только в приватных каналах.';
+            msgText = '/info ' + this.i18nWorkingOnlyInPrivate + '.';
         }
         const messageTemplate: IMessage = {
             text: msgText,
@@ -65,17 +71,15 @@ class InfoCommand implements ISlashCommand {
     }
 
     // @ts-ignore
-    private async getuserlist(roomid: string, role: string, http: IHttp, token: string, userid: string): Promise<Array<string>> {
-        this.applogger.debug('Start: roomid=', roomid, ' role=', role, ';');
-        const response: IHttpResponse = await http.get( 'http://192.168.1.249:3000/api/v1/roles.getUsersInRole?role=' + role + '&roomId=' + roomid , {
+    private async getuserlist(roomid: string, role: string, http: IHttp, token: string, userid: string, hosturl: string): Promise<Array<string>> {
+        const response: IHttpResponse = await http.get( hosturl + '/api/v1/roles.getUsersInRole?role=' + role + '&roomId=' + roomid , {
             // tslint:disable-next-line:max-line-length
             headers : {'Content-Type' : 'application/json' , 'X-Auth-Token' : token , 'X-User-Id' : userid },
         });
-        this.applogger.debug('Stop: roomid=', roomid, ' role=', role, ';');
         let result : Array<string> = Array();
         if (response.statusCode === 200) {
             const json = JSON.parse(response.content as string);
-            if ((json.success === 'true') && (json.total > 0)) {
+            if ((json.success === true) && (json.total > 0)) {
                 const users = json.users;
                 for (const user of users) {
                     result.push(user.username);
@@ -107,6 +111,9 @@ export  class ChatInfoApp extends App {
         });
         await configuration.settings.provideSetting({
             i18nLabel: 'user_id', packageValue: '', public: false, required: true, type: SettingType.STRING, id: 'id_info_setting_uid',
+        });
+        await configuration.settings.provideSetting({
+            i18nLabel: 'host_url', packageValue: '', public: false, required: true, type: SettingType.STRING, id: 'id_info_setting_host_url',
         });
     }
 }
